d85e74477d433ea49e0eedc9766110cfa306e42d
Angel Adell <angeldeniseadell@gmail.com>
2023-11-26 17:24:03 -0500

    feat: add card/ search functionality

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict";
 (() => {
     // users input from search bar
-    const searchInput = document.querySelector("[data-search]")
-    // coffee suggestion button
+    const searchInput = document.querySelector("[data-search]");    // coffee suggestion button
     const btn = document.querySelector("#submit-coffeeSuggest")
     // coffee suggestions index to limit amount of suggested coffees
     let index = 0
     // coffee list
     const coffeesList = document.querySelector('#coffees')
     // coffee suggestion input
-    let input = document.getElementById("coffeeAdd")
+    let input = document.getElementById("coffeeAdd");
 
 
 // button event for adding coffee suggestion
@@ -34,29 +33,18 @@
 
     // function that generates an HTML string representing a "coffee card" based on the information of a given coffee object.
     function renderCoffee(coffee) {
-        let html = '<div class="card" style="width: 18rem;">';
-        html += `<img src="images/IMG-20210719-WA0004_1_900x.webp" class="card-img-top">`;
+        let html = '<div class="card d-flex col mb-4">';
+        html += `<img src="images/${coffee.image}" class="coffee-img" alt="Coffee Image">`;
         html += `<div class="card-body">${coffee.name}</div>`;
-        html += `<div>${coffee.roast}</div>`;
+        html += `<p>${coffee.roast}</p>`;
         html += '</div>';
 
         return html;
     }
-    // this function responsible for rendering coffee objects on the webpage.
-    function renderCoffees(coffees) {
-        let html = '';
-        coffees.forEach(coffee => {
-            const cardHtml = renderCoffee(coffee);
-            coffeeCardsContainer.innerHTML += cardHtml;
-        });
-        return html
-    }
 
-    const value = searchInput.value.toLowerCase();
-    const selectedRoast = document.querySelector('#roast-selection').value.toLowerCase();
     // filters through the coffees array
     function updateCoffees(event) {
-        event.preventDefault();
+      if (event) {event.preventDefault();}
 
         const value = searchInput.value.toLowerCase();
         const selectedRoast = document.querySelector('#roast-selection').value.toLowerCase();
@@ -68,7 +56,6 @@
             const isNameMatch = coffee.name.toLowerCase().includes(value);
             const isRoastMatch = coffee.roast.toLowerCase().includes(selectedRoast);
             const isAllMatch = selectedRoast === 'all';
-
             if (isNameMatch && (isRoastMatch || isAllMatch)) {
                 const cardHtml = renderCoffee(coffee);
                 coffeeCardsContainer.innerHTML += cardHtml;
@@ -79,43 +66,31 @@
 
 
     // combo function event for inputing a coffee search and or submitting
-    function init(event) {
-        event.preventDefault()
+    function init() {
         searchInput.addEventListener("input", updateCoffees);
-        document.querySelector('#submit').addEventListener('click', updateCoffees);
-        tbody.innerHTML = renderCoffees(coffees);
+        document.querySelector("#roast-selection").addEventListener('input', updateCoffees);
+        document.querySelector('#searchRoast').addEventListener('submit', updateCoffees);
+        updateCoffees();
     }
-
-
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 
-
     const coffees = [
-        {id: 1, name: 'Light City', roast: 'light'},
-        {id: 2, name: 'Half City', roast: 'light' },
-        {id: 3, name: 'Cinnamon', roast: 'light' },
-        {id: 4, name: 'City', roast: 'medium' },
-        {id: 5, name: 'American', roast: 'medium' },
-        {id: 6, name: 'Breakfast', roast: 'medium' },
-        {id: 7, name: 'High', roast: 'dark' },
-        {id: 8, name: 'Continental', roast: 'dark' },
-        {id: 9, name: 'New Orleans', roast: 'dark' },
-        {id: 10, name: 'European', roast: 'dark' },
-        {id: 11, name: 'Espresso', roast: 'dark' },
-        {id: 12, name: 'Viennese', roast: 'dark' },
-        {id: 13, name: 'Italian', roast: 'dark' },
-        {id: 14, name: 'French', roast: 'dark' },
+        {id: 1, name: 'Light City', roast: 'light', image: 'IMG-20210719-WA0004_1_900x.webp'},
+        {id: 2, name: 'Half City', roast: 'light', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 3, name: 'Cinnamon', roast: 'light', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 4, name: 'City', roast: 'medium', image: 'IMG-20210719-WA0004_1_900x.webp'},
+        {id: 5, name: 'American', roast: 'medium', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 6, name: 'Breakfast', roast: 'medium', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 7, name: 'High', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 8, name: 'Continental', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 9, name: 'New Orleans', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 10, name: 'European', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 11, name: 'Espresso', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 12, name: 'Viennese', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 13, name: 'Italian', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
+        {id: 14, name: 'French', roast: 'dark', image: 'IMG-20210719-WA0004_1_900x.webp' },
     ];
-
-
-
-    const tbody = document.querySelector('#coffees');
-    const submitButton = document.querySelector('form').addEventListener('submit', updateCoffees);
-
-    tbody.innerHTML = renderCoffees(coffees);
-
-    submitButton.addEventListener('click', updateCoffees);
 
 
     init();
